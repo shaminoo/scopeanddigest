39586b62a095acff67f0be4cbffab0b1292ae305
Shaminoo Kapoor <shkapoor@microsoft.com>
2017-11-10 16:37:38 -0800

    updated readme

--- mainController.js
+++ mainController.js
@@ -1,24 +1,17 @@
 var myApp = angular.module("myApp", []);
-myApp.controller('mainController', function($scope) {
-    var vm = this;
-    this.force = 30;
-    this.forceInUSD = this.force * 10;
-    this.resetForce = function() {
-        this.force = 0;
-    }
-    var times = 0;
-    $scope.$watch(function() {
-        console.log('watcher was called', times);
-        times++;
-        return 0;
-    }, function() {});
-
-    // $scope.$watch('vm.force', function() {
-    //     vm.forceInUSD = vm.force * 10;
-    // })
-});
-myApp.controller('internalController', function($scope) {
-    //$scope.intrnl = this;
-    this.main = $scope.$parent.vm;
-    this.secret = "secret" + $scope.$parent.vm.force;
+myApp.controller('mainController', function($scope, $rootScope) {
+   $scope.myText = "myText";
+   $scope.helloWorld = "hello";
+   $scope.$watch('helloWorld', function(newValue, oldValue) {
+        console.log("helloWorld newValue" + oldValue);
+        console.log("helloWorld oldValue" + newValue);
+    });
+   $scope.$watch('myText', function() {
+       console.log("myText changed");
+       $scope.helloWorld = "helloWorld";
+       console.log("helloWorld changed");
+   });
+   $rootScope.$watch(function() {
+       console.log("digest running");
+   });
 });
--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
-#following this video https://www.lynda.com/Angular-tutorials/scope-watch/603869/636230-4.html?autoplay=true
-#1. undestand scope inheritance, how child can access parent
-#2. understood using controller instance on scope
-#3. remove it from scope and use controller as
-#4. use vm for both main and internal controller and how to access parent scope from child
-#5. how to use $watch with variables and functions
-#6. using div to show how watch works using innherhtml
-#7. call digest loop manually
+#1. followed https://www.youtube.com/watch?v=SYuc1oSjhgY to understand how to use watch
+#2. use rootscope watch
+#3. logging start of digest cycle using rootscope
+#4. understanding digest loop, how many times it runs
+#5. runs twice minimum, if any scope variable changed in one listener watch is run again
+#6. digest loop starts on dom events, and other events, and that runs watch
+#7. after watches are complete, dom is updated in the end.
+#8. 
